f96e466db5d6877f7c3be2e3ccf49316d8dda91a
Ubuntu <arildlil@protonmail.com>
2019-02-08 14:23:52 +0000

    feat Added argparse support to 'convert.py' and added code for converting function calls to ADD_TEST, and for adding KTF_CONTEXT_ADD in the main function.

--- test_xarray_rewrite/convert.py
+++ test_xarray_rewrite/convert.py
@@ -1,13 +1,21 @@
-import re
+import re, os, argparse
 
-filename = "kernel/test_xarray_rewrite.c"
+default_filename = "/home/ubuntu/src/test_xarray_rewrite/kernel/test_xarray_rewrite.c"
 
-with open(filename, 'r') as f:
+parser = argparse.ArgumentParser(
+    description='A python script hand crafted for converting "test_xarray.c" to KTF.')
+parser.add_argument('-f', '--file', action='store', default=default_filename)
+args = parser.parse_args()
+print(args)
+print(args.file)
+
+with open(args.file, 'r') as f:
     data = f.read()
+
 
 #print(data)
 
-def convert_to_test_macro(text):
+def convert_func_signature_to_test_macro(text):
     """
     Converts regular test functions on the format
     "static noinline void check_XXX(struct xarray *xa)
@@ -17,16 +25,41 @@ def convert_to_test_macro(text):
     signature, as this function also adds context related code to 
     the beginning of the function.
     """
-    reg = r"static noinline void (check_.*)\((struct xarray [*]xa|void)\)\n\s*({)"
+    reg = r'static noinline void (check_.*)\((struct xarray [*]xa|void)\)\n\s*({)'
     ctx_code = r"""
         struct array_context *actx = KTF_CONTEXT_GET("array", struct array_context);
         struct xarray *xa = actx->xa;
         """
-    res_pat = r"TEST(test_xarray_rewrite, \g<1>)\n{" + ctx_code
-    res = re.sub(reg, res_pat, text, 0, re.MULTILINE)
-    return res
+    res_pat = r'TEST(test_xarray_rewrite, \g<1>)\n{' + ctx_code
+    return re.sub(reg, res_pat, text, 0, re.MULTILINE)
+
+def convert_func_calls_to_add_test_macro(text):
+    """
+    Converts calls to regular test functions into calls
+    to ADD_TEST instead. It aims to only capture function 
+    calls inside the main function 'xarray_checks', by not
+    allowing any deviations in terms of arguments or numbers
+    in function names.
+    """
+    reg = r'(check_[a-z_]*)\((&array)*\);\s*'
+    res_pat = r'ADD_TEST(\g<1>);\n\t'
+    return re.sub(reg, res_pat, text, 0, re.MULTILINE)
+
+def add_ktf_context_add_to_main(text):
+    """
+    Adds a call to KTF_CONTEXT_ADD to the beginning of the
+    main function.
+    """
+    reg = r'(static int xarray_checks\(void\)\s*?{\s*)'
+    ctx_code = r'KTF_CONTEXT_ADD(&cxa.k, "array");'
+    res_pat = r'\g<1>{}\n\n\t'.format(ctx_code)
+    return re.sub(reg, res_pat, text, 0, re.MULTILINE)
     
-print(convert_to_test_macro(data))
+
+print(
+    add_ktf_context_add_to_main(
+        convert_func_signature_to_test_macro(
+            convert_func_calls_to_add_test_macro(data))))
 
 #for x in reg.finditer(data):
 #    print(x.groups())
--- test_xarray_rewrite/kernel/test_xarray_rewrite.c
+++ test_xarray_rewrite/kernel/test_xarray_rewrite.c
@@ -81,7 +81,9 @@ static void *xa_store_order(struct xarray *xa, unsigned long index,
 	return curr;
 }
 	
-TEST(test_xarray_rewrite, check_xa_err) {
+//TEST(test_xarray_rewrite, check_xa_err) {
+static noinline void check_xa_err(struct xarray *xa)
+{
 	struct array_context *actx = KTF_CONTEXT_GET("array", struct array_context);
 	struct xarray *xa = actx->xa;
 	
@@ -1223,15 +1225,11 @@ static DEFINE_XARRAY(array);
 // ADDED
 static struct array_context cxa = { .xa = &array };
 //
+// KTF_CONTEXT_ADD(&cxa.k, "array");
 
 static int xarray_checks(void)
 {
-	KTF_CONTEXT_ADD(&cxa.k, "array");
-
-	// ADDED
-	//check_xa_err(&array);
-	ADD_TEST(check_xa_err);
-	
+	check_xa_err(&array);
 	check_xas_retry(&array);
 	check_xa_load(&array);
 	check_xa_mark(&array);
@@ -1256,28 +1254,28 @@ static int xarray_checks(void)
 	// DONE
 
 	/*
-	check_xa_err(&array);
-	check_xas_retry(&array);
-	check_xa_load(&array);m
-	check_xa_mark(&array);
-	check_xa_shrink(&array);
-	check_xas_erase(&array);
-	check_cmpxchg(&array);
-	check_reserve(&array);
-	check_multi_store(&array);
-	check_xa_alloc();
-	check_find(&array);
-	check_find_entry(&array);
-	check_account(&array);
-	check_destroy(&array);
-	check_move(&array);
-	check_create_range(&array);
-	check_store_range(&array);
-	check_store_iter(&array);
+	+check_xa_err(&array);
+	+check_xas_retry(&array);
+	+check_xa_load(&array);
+	+check_xa_mark(&array);
+	+check_xa_shrink(&array);
+	+check_xas_erase(&array);
+	+check_cmpxchg(&array);
+	+check_reserve(&array);
+	+check_multi_store(&array);
+	+check_xa_alloc();
+	+check_find(&array);
+	+check_find_entry(&array);
+	+check_account(&array);
+	+check_destroy(&array);
+	+check_move(&array);
+	+check_create_range(&array);
+	+check_store_range(&array);
+	+check_store_iter(&array);
 
-	check_workingset(&array, 0);
-	check_workingset(&array, 64);
-	check_workingset(&array, 4096);
+	+check_workingset(&array, 0);
+	+check_workingset(&array, 64);
+	+check_workingset(&array, 4096);
 
 	printk("XArray: %u of %u tests passed\n", tests_passed, tests_run);
 	return (tests_run == tests_passed) ? 0 : -EINVAL;
