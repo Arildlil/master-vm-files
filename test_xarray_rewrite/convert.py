a0ed97333e6a3bcc351038680568601f684505e1
Ubuntu <arildlil@protonmail.com>
2019-02-27 11:44:05 +0000

    feat Made the convert script be able to (apparently) convert the whole test_xarray.c file.

--- test_xarray_rewrite/convert.py
+++ test_xarray_rewrite/convert.py
@@ -1,5 +1,12 @@
 import re, os, argparse, sys
 from pprint import pprint
+from collections import namedtuple
+from string import whitespace
+
+ConversionRules = namedtuple('ConversionRules', 
+    'test_funcs init exit include new_types boilerplate suite_name ctx_args_def \
+    cmn_test_args_call extra_dummy_args_call blacklist replacements')
+
 
 # Argsparse related code
 # -------------------------------------------------------------------
@@ -10,7 +17,6 @@ parser = argparse.ArgumentParser(
     description='A python script hand crafted for converting "test_xarray.c" to KTF.')
 parser.add_argument('-f', '--file', action='store', default=default_filename)
 parser.add_argument('-o', '--out', action='store')
-parser.add_argument('-t', '--testfuncs', nargs='+', type=str)
 args = parser.parse_args()
 
 with open(args.file, 'r') as f:
@@ -20,47 +26,70 @@ with open(args.file, 'r') as f:
 # Code local for one specific test file. This could be supplied in
 # other ways, but currently it will be written here.
 # -------------------------------------------------------------------
-init_code = r"""static struct array_context cxa = { .xa = &array };
+test_xarray_rules = ConversionRules(
+    test_funcs =
+    """
+    check_xa_err 
+    check_xas_retry
+	check_xa_load
+	check_xa_mark 
+    check_xa_shrink 
+	check_xas_erase 
+	check_cmpxchg 
+	check_reserve 
+	check_multi_store 
+	check_xa_alloc 
+	check_find 
+	check_find_entry 
+	check_account 
+	check_destroy 
+	check_move 
+	check_create_range 
+	check_store_range 
+	check_store_iter
+	check_workingset
+    """,
+    init = 
+r"""static struct array_context cxa = { .xa = &array };
 
 KTF_INIT();
 
 \g<1>
     \tKTF_CONTEXT_ADD(&cxa.k, "array");
-"""
-
-exit_code = r"""\g<1>
+""", 
+    exit = 
+r"""\g<1>
         struct ktf_context *pctx = KTF_CONTEXT_FIND("data");
         KTF_CONTEXT_REMOVE(pctx);
         
-        KTF_CLEANUP();"""
-
-include_code = r"""\g<1>
+        KTF_CLEANUP();
+""",
+    include = r"""\g<1>
 #include "ktf.h" 
 
-"""
-
-type_def_code = r"""\g<1>
+""",
+    new_types = r"""\g<1>
 
 struct array_context {
     struct ktf_context k;
     struct xarray *xa;
 };
 
-"""
-
-boilerplate_test_code = r"""\g<1>
-
+""",
+    boilerplate = 
+r"""\g<1>
         struct array_context *actx = KTF_CONTEXT_GET("array", struct array_context);
         struct xarray *xa = actx->xa;
-        """
-
-test_suite_name = "test_xarray_rewrite"
-
-context_args = "struct xarray [*]xa|void"
-
-common_call_args = "&array"
-
-common_call_multi_arg_replace = "xa"
+    """,
+    suite_name = "test_xarray_rewrite",
+    ctx_args_def = "struct xarray [*]xa|void",
+    cmn_test_args_call = "&array",
+    extra_dummy_args_call = "xa",
+    blacklist = ["test_update_node", "xa_load", "xa_alloc", "xas_retry", "xa_err"],
+    replacements = [
+        (r"(^\s*)(XA_BUG_ON[(]xa, *)", "\g<1>EXPECT_FALSE(")
+    ]
+)
 
 
 
@@ -72,6 +101,12 @@ def printr(matches):
 
 
 
+    """
+    def __init__(self, text, test_func_names="", outfile_name, test_suite_name="", 
+        ignore_func_names="", init_code="", exit_code="", include_code="", type_def_code="",
+        boilerplate_test_code="", ctx_args="", common_call_args="",
+        common_call_multi_arg_replace="", assertion_replacements="", debug=True):
+    """
 class CurrentState(object):
     """
     Class used to store and handle parameters from the user,
@@ -80,51 +115,53 @@ class CurrentState(object):
     functions, this can be done once and stored here for future use.
     """
 
-    def __init__(self, text, test_func_names, outfile_name, test_suite_name, 
-        init_code="", exit_code="", include_code="", type_def_code="",
-        boilerplate_test_code="", ctx_args="", common_call_args="",
-        common_call_multi_arg_replace="", debug=True):
-
+    def __init__(self, text, outfile_name, rules, debug=False):
 
         # Argument handling:
         # ------------------
         # All the contents of the source file.
         self._text = text
 
-        # Names of all the functions to convert to TEST.
-        self._test_function_names = test_func_names
-
         # The name of the file to write output to.
         self._outfile_name = outfile_name
 
-        # The name of the test suite itself.
-        self._test_suite_name = test_suite_name
+        # Names of all the functions to convert to TEST.
+        self._test_function_names = rules.test_funcs
 
         # Code to be added to the init function of the module, if any.
-        self._init_code = init_code
+        self._init_code = rules.init
         
         # Code to be added to the exit function of the module, if any.
-        self._exit_code = exit_code
+        self._exit_code = rules.exit
 
         # Code for header inclusion
-        self._include_code = include_code
+        self._include_code = rules.include
 
         # Code for definition of structs and typedefs.
-        self._type_def_code = type_def_code
+        self._type_def_code = rules.new_types
                 
         # Context code that should be added to the beginning
         # of every TEST function.
-        self._boilerplate_test_code = boilerplate_test_code
+        self._boilerplate_test_code = rules.boilerplate
+
+        # The name of the test suite itself.
+        self._test_suite_name = rules.suite_name
 
         # Common arguments which should be moved into a context instead.
-        self._context_args = context_args
+        self._context_args = rules.ctx_args_def
 
         # Common arguments which are often used when calling test functions.
-        self._common_call_args = common_call_args
+        self._common_call_args = rules.cmn_test_args_call
 
         # Replacement for _common_call_args for multi argument function calls
         # that needs to be put in a dummy function.
-        self._common_call_multi_arg_replace = common_call_multi_arg_replace
+        self._common_call_multi_arg_replace = rules.extra_dummy_args_call
+
+        # The names of functions to be ignored.
+        self._ignore_func_names = rules.blacklist
+
+        # Specifies which assertions to convert to which KTF assertions.
+        self._assertion_replacements = rules.replacements
 
         self._debug = debug
 
@@ -148,6 +185,8 @@ class CurrentState(object):
         self._module_exit_name = ""
 
         self._extra_parameters = "struct ktf_test *self"
+        self._extra_parameters_calls = "self"
+        self._extra_parameters_calls_comma = "self, "
         self._test_macro_result = "TEST({suite_name}, {test_name}) {{\n"
         self._dummy_function_name = "{func_name}_{counter}_"
         self._dummy_function_body = "TEST({suite_name}, {dummy_name}) {{\n\t{call}\n}}"
@@ -155,6 +194,8 @@ class CurrentState(object):
         self._add_test_call = "ADD_TEST({func_name});"
         self._dummy_function_internal_call = "{func_name}({alt_args}, {rest_args});" # common_call_multi_arg_replace
         self._extra_parameters_result = "{sig}{ext_params}{opt_comma}{args}{end}\n{{"
+        self._extra_ktf_args_calls = "{func_name}({new_args}{old_arg_char}"
+        self._extra_ktf_args_calls_no_args = "{func_name}({new_args})"
         self._wrapped_multi_arg_function_calls = "{func_name}({common_args}{extra_args});"
 
         # Regexes used in this class
@@ -171,6 +212,13 @@ class CurrentState(object):
             'statics_with_context_and_extra_args': "((static *(noinline)*? *([a-z_*0-9 ]+?) *([a-zA-Z_0-9]*)\()({ctx_args})(.*)(\))\s*{{)",
             'test_macro_function': "(TEST(.*?), *(.*?) *{)",
             'function_calls_common_args': "(([a-zA-Z0-9_]*)\(({common_args})*\);)",
+            #'function_calls_all_args': "(([\w_]+)(\()(.*)(\)))([,; \s)])(?!{)",
+            #'function_calls_all_args': "((([\w_]+)(\()(.*))(\)[,; \s)](?!{)))",
+            # We exploit the fact that this should be used AFTER the KTF specific
+            # arguments have been added.
+            'function_calls_with_args': "(((\w)+)(\()((?!struct)(?!\))))",
+            'find_exact_match': "({pattern})",
+            'function_calls_without_args': "(((\w)+)(\(\)))",
             'multi_arg_test_function_calls': "(([a-zA-Z0-9_]*)\(({common_args}), *(.*?)\));",
         }
             
@@ -292,6 +340,7 @@ class CurrentState(object):
         """
         test_name = matches.group(5)
         if test_name in self._test_function_names:
+            pprint("(def repl) Replacing " + str(test_name))
             return self._test_macro_result.format(
                 suite_name=self._test_suite_name, \
                 test_name=test_name)
@@ -333,6 +382,8 @@ class CurrentState(object):
                 suite_name=self._test_suite_name, dummy_name=dummy_func_name, 
                 call=modified_call)
             add_test_call = self._add_test_call.format(func_name=dummy_func_name)
+
+            #print("(1 m.arg) dummy_func_name: " + dummy_func_name)
             """
             print("\tdummy_func_name: " + dummy_func_name)
             print("\t\told call: " + str(matches.groups()))
@@ -355,8 +406,12 @@ class CurrentState(object):
         Returns True if the test name refers to a helper function
         or a test function with additional arguments.
         """
-        return test_name in self._local_helper_function_names or \
-          test_name in self._test_function_names
+        print("_local_helper_func_names: ")
+        for h in self._local_helper_function_names:
+            print(h)
+        return test_name not in self._ignore_func_names and \
+            (test_name in self._local_helper_function_names or \
+            test_name in self._test_function_names)
 
     def _add_extra_args_if_valid_definition(self, matches):
         """
@@ -373,11 +428,11 @@ class CurrentState(object):
             old_args = ""
         if self._is_helper_or_multi_arg_test_function(test_name):
             #pprint("Modded (1): " + str(matches.group(1)))
+            #pprint("(2 v.def): " + str(matches.group(1)))
             return self._extra_parameters_result.format(
                 sig=function_signature, ext_params=self._extra_parameters, 
                 opt_comma=optional_comma, args=old_args, end=rest)
         else:
-            pprint("(1): " + str(matches.group(1)))
             return matches.group(1)
 
     def _add_extra_args_if_valid_call(self, matches):
@@ -386,10 +441,50 @@ class CurrentState(object):
         either a helper function or a multiple argument function
         "marked" for conversion.
         """
-        # TODO: Continue from here!
-        # Current regex: (([a-zA-Z0-9_]+)\((.*))\);
-        pprint("CALL: " + str(matches.groups()))
+        func_name = matches.group(2)
+        if self._is_helper_or_multi_arg_test_function(func_name):
+            #pprint("(3 v.call): " + str(func_name))
+            #self._extra_ktf_args_calls = "{func_name}({new_args}{old_arg_char}"
+            extra_params = self._extra_parameters_calls_comma
+            
+            new_call = self._extra_ktf_args_calls.format(
+                func_name=func_name, new_args=extra_params,
+                old_arg_char="") 
+            #pprint("    new_call: " + new_call)
+            return new_call
+        else:
+            return matches.group(1)
+
+    def _add_extra_args_if_valid_call_no_args(self, matches):
+        """
+        Same as above, expect it's only used for function calls
+        without any exisiting arguments.
+        """
+        func_name = matches.group(2)
+        if self._is_helper_or_multi_arg_test_function(func_name):
+            #pprint("(4 v.call): " + str(func_name))
+            extra_params = self._extra_parameters_calls
+
+            new_call = self._extra_ktf_args_calls_no_args.format(
+                func_name=func_name, new_args=extra_params)    # "{func_name}()"
+            return new_call
+        else:
+            return matches.group(1)
+
+    def _convert_assertions_to_KTF(self, matches):
+        """
+        Replaces assertions from the self._assertion_replacements
+        list of tuples to the corresponding right KTF assertion.
+        """
+        func_name_with_left_par = matches.group(1)
+        #[pprint(item[1]) for item in self._assertion_replacements if item[0]+"(" == func_name_with_left_par]
+        for item in self._assertion_replacements:
+            if item[0]+"(" == func_name_with_left_par:
+                return item[1]+"("
         return matches.group(1)
+
+        #[pprint(item) for item in self._assertion_replacements if item[0] == func_name_with_left_par]
+        #print("\t" + str(matches.group(1)))
 
     def _sub(self, reg, result):
         """
@@ -465,7 +560,7 @@ class CurrentState(object):
             for dummy_tuple in self._dummy_functions_to_add:
                 self._sub(
                     self._regexes['specific_static_function'].format(
-                        func_name=dummy_tuple[0]),
+                        func_name=self._module_init_name),
                     self._dummy_function_result.format(
                         dummy_body=dummy_tuple[1], orig_func="\g<1>"))
         else:
@@ -482,7 +577,7 @@ class CurrentState(object):
                 common_args=self._common_call_args),
             self._replace_if_valid_test_function_call)
 
-    def add_extra_parameters_to_helpers_and_multi_arg_tests(self):
+    def add_extra_parameters_to_helpers_and_multi_arg_defs(self):
         """
         Adds a KTF self argument to all helper functions.
         """
@@ -493,10 +588,23 @@ class CurrentState(object):
     def add_self_argument_to_helper_calls(self):
         """
         Adds an extra self argument to calls on helper functions.
+        Does two rounds of substitution to cover the very few cases
+        where there are no arguments. 
         """
-        return self._sub(
-            self._regexes[''],
+        self._sub(
+            self._regexes['function_calls_with_args'],
             self._add_extra_args_if_valid_call)
+        return self._sub(
+            self._regexes['function_calls_without_args'],
+            self._add_extra_args_if_valid_call_no_args)
+
+    def convert_assertions(self):
+        """
+        Converts assertions calls to KTF assertions.
+        """
+        for pattern in self._assertion_replacements:
+            self._text = re.sub(pattern[0], pattern[1], self._text, flags=re.MULTILINE)
+        return self
 
     def add_boilerplate_test_code(self):
         """
@@ -518,13 +626,15 @@ class CurrentState(object):
             f.write(self._text)
 
 
+# def __init(self, text, outfile_name, rules, debug=False):
 
-
-
-
+state = CurrentState(data, args.out, test_xarray_rules, False)
+"""
 state = CurrentState(data, args.testfuncs, args.out, test_suite_name, \
-    init_code, exit_code, include_code, type_def_code, boilerplate_test_code, \
-    context_args, common_call_args, common_call_multi_arg_replace, debug=False)
+    ignore_func_names, init_code, exit_code, include_code, type_def_code, \
+    boilerplate_test_code, context_args, common_call_args, \
+    common_call_multi_arg_replace, assertion_replacements, debug=False)
+    """
 
 state.add_include_code() \
     .add_init_code_to_main() \
@@ -534,7 +644,9 @@ state.add_include_code() \
     .convert_to_test_extra_args() \
     .convert_calls_to_add_test() \
     .add_boilerplate_test_code() \
-    .add_extra_parameters_to_helpers_and_multi_arg_tests() \
+    .add_extra_parameters_to_helpers_and_multi_arg_defs() \
+    .add_self_argument_to_helper_calls() \
+    .convert_assertions() \
     .result()
 
 print(state._text)
